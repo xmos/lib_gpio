9b9247002fa7a1061ea49c153b3fe127d31550d2
David Lacey <david.lacey@xmos.com>
2014-10-31 14:41:57 +0000

    Update API to allow eventing on input ports

--- examples/gpio_flashing_leds/src/main.xc
+++ examples/gpio_flashing_leds/src/main.xc
@@ -17,11 +17,11 @@ void flash_leds(client output_gpio_if led1, client output_gpio_if led2)
 }
 
 int main() {
-  interface output_gpio_if i_led1, i_led2;
+  interface output_gpio_if i_led1[1], i_led2[1];
   par {
-    output_gpio(i_led1, p_led1);
-    output_gpio(i_led2, p_led2);
-    flash_leds(i_led1, i_led2);
+    output_gpio(i_led1, 1, p_led1, null);
+    output_gpio(i_led2, 1, p_led2, null);
+    flash_leds(i_led1[0], i_led2[0]);
   }
   return 0;
 }
--- lib_gpio/api/gpio.h
+++ lib_gpio/api/gpio.h
@@ -28,6 +28,25 @@ typedef interface input_gpio_if
    *           The rest of the value will be zero extended.
    */
  unsigned input_and_timestamp(gpio_time_t &timestamp);
+
+ /** Request an event when the pin is a certain value.
+  *
+  *  This function will cause a notification to occur when the pins
+  *  match the specified value.
+  *
+  *  \param val   The value to match.
+  */
+ [[clears_notification]]
+ void event_when_pins_eq(unsigned val);
+
+ /** A pin event has occurred.
+  *
+  *  This notification will occur when a pin event has occurred.
+  *  Events can be requested using the event_when_pins_eq() call.
+  */
+ [[notification]]
+ slave void event(void);
+
 } input_gpio_if;
 
 /** This interface provides access to a GPIO that can perform output
@@ -54,65 +73,81 @@ typedef interface output_gpio_if
 } output_gpio_if;
 
 
-
-/** This interface provides access to a GPIO that can perform input and output
-    operations. */
-typedef interface inout_gpio_if
-{
-  /** Perform an input on a GPIO
-   *
-   *  \returns The value input from the port in the least significant bit.
-   *           The rest of the value will be zero extended.
-   */
- unsigned input(void);
-
-  /** Perform an input on a GPIO and get a timestamp
-   *
-   *
-   *  \param   timestamp  This pass-by-reference parameter will be set
-   *                      to the time the value was input. This timestamp
-   *                      is on a timebase relative to the GPIO.
-   *
-   *  \returns The value input from the port in the least significant bit.
-   *           The rest of the value will be zero extended.
-   */
- unsigned input_and_timestamp(gpio_time_t &timestamp);
-
-  /** Perform an output on a GPIO.
-   *
-   *  \param data  The value to be output. The least significant bit
-   *               represents the 1-bit value to be output.
-   */
- void output(unsigned data);
-
-  /** Perform an output on a GPIO and get a timestamp of when the output
-   *  occurs.
-   *
-   *  \param data  The value to be output. The least significant bit
-   *               represents the 1-bit value to be output.
-   *
-   *  \returns     The time the value was input. This timestamp
-   *               is on a timebase relative to the GPIO.
-   */
- gpio_time_t output_and_timestamp(unsigned data);
-} inout_gpio_if;
-
-
+/* Task that splits a multibit port into several 1 bit GPIO interfaces.
+ *
+ * This component allows other tasks to access the individual bits of
+ * a multi-bit output port.
+ *
+ * \param   i         The array of interfaces to connect to other tasks.
+ * \param   n         The number of interfaces connected.
+ * \param   p         The output port to be split.
+ * \param   pin_map   This array maps the connected interfaces to the pin
+ *                    of the port. For example, if 3 clients are connected
+ *                    to split a 8-bit port and the array {2,5,3} is supplied.
+ *                    Then bit 2 will go to interface 0, bit 5 to inteface 1
+ *                    and bit 3 to inteface 2. If null is supplied for this
+ *                    argument then the pin map is assumed to be {0,1,2...}.
+ */
 [[distributable]]
-void inout_gpio(server inout_gpio_if i, port p);
+void output_gpio(server output_gpio_if i[n], static const size_t n, port p,
+                 char (&?pin_map)[n]);
 
+/* Task that splits a multibit input port into several 1 bit GPIO interfaces
+ * (no events).
+ *
+ * This component allows other tasks to access the individual bits of
+ * a multi-bit input port. It does not support events but is distributable so
+ * requires no specific logical core to run on. If the event_when_pins_eq()
+ * function is called then the component will trap.
+ *
+ * \param   i         The array of interfaces to connect to other tasks.
+ * \param   n         The number of interfaces connected.
+ * \param   p         The input port to be split.
+ * \param   pin_map   This array maps the connected interfaces to the pin
+ *                    of the port. For example, if 3 clients are connected
+ *                    to split a 8-bit port and the array {2,5,3} is supplied.
+ *                    Then bit 2 will go to interface 0, bit 5 to inteface 1
+ *                    and bit 3 to inteface 2. If null is supplied for this
+ *                    argument then the pin map is assumed to be {0,1,2...}.
+ */
 [[distributable]]
-void input_gpio(server input_gpio_if i, port p);
+void input_gpio(server input_gpio_if i[n], static const size_t n, port p,
+                char (&?pin_map)[n]);
 
-[[distributable]]
-void output_gpio(server output_gpio_if i, port p);
+/* Task that splits a multibit input port into several 1 bit GPIO interfaces
+ * (with events).
+ *
+ * This component allows other tasks to access the individual bits of
+ * a multi-bit input port. It does support events so requires a logical
+ * core to run on (but can be combined with other tasks on the same core).
+ *
+ * \param   i         The array of interfaces to connect to other tasks.
+ * \param   n         The number of interfaces connected.
+ * \param   p         The input port to be split.
+ * \param   pin_map   This array maps the connected interfaces to the pin
+ *                    of the port. For example, if 3 clients are connected
+ *                    to split a 8-bit port and the array {2,5,3} is supplied.
+ *                    Then bit 2 will go to interface 0, bit 5 to inteface 1
+ *                    and bit 3 to inteface 2. If null is supplied for this
+ *                    argument then the pin map is assumed to be {0,1,2...}.
+ */
+[[combinable]]
+void input_gpio_with_events(server input_gpio_if i[n],
+                            static const size_t n,
+                            port p,
+                            char (&?pin_map)[n]);
 
-
-[[distributable]]
-void multibit_output_gpio(server output_gpio_if i[n], size_t n, unsigned pin_map[n], port p_gpio);
-
-[[distributable]]
-void multibit_input_gpio(server input_gpio_if i[n], size_t n, port p, unsigned pin_map[n]);
+/* Conver a 1-bit port to a single 1-bit GPIO interface.
+ *
+ * This component allows other tasks to access a 1 bit port as a GPIO
+ * interface. It is more efficient that using input_gpio_with_events() for the
+ * restricted case where a 1-bit port is used.
+ *
+ * \param   i         The interface to connect to other tasks.
+ * \param   p         The input port.
+ */
+[[combinable]]
+void input_gpio_1bit_with_events(server input_gpio_if i, port p);
 
 #endif
 #endif // __gpio_h__
