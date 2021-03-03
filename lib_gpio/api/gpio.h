e728a006b5fcce4abb280a124eca80fa5b7200e7
Daniel Pieczko <danielpieczko@xmos.com>
2021-03-03 08:33:46 +0000

    Add licence references in source files

--- lib_gpio/api/gpio.h
+++ lib_gpio/api/gpio.h
@@ -1,4 +1,5 @@
-// Copyright (c) 2014-2016, XMOS Ltd, All rights reserved
+// Copyright (c) 2014-2021, XMOS Ltd, All rights reserved
+// This software is available under the terms provided in LICENSE.txt.
 #ifndef __gpio_h__
 #define __gpio_h__
 #include <stdint.h>
